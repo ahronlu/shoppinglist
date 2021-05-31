c649df8cb09ddd38778635a4d95298f0b28af381
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-05-31 07:17:59 +0300

    Update Item.js

--- models/Item.js
+++ models/Item.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
 // Create Schema
-const ItemSchema = new Schema({
+const ItemSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
   name: {
     type: String,
     required: true,
