5cc4219f7ac75cb2bc57d9ac548ec455657866a4
koftov <ahronlu@gmail.com>
2021-08-15 00:02:30 +0300

    react-hook-form

--- client/src/components/auth/LoginModal.js
+++ client/src/components/auth/LoginModal.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
 import {
   Button,
   Modal,
@@ -8,17 +10,24 @@ import {
   FormGroup,
   Label,
   Input,
+  FormFeedback,
   NavLink,
   Alert,
 } from "reactstrap";
-import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../actions/authActions";
 import { clearErrors } from "../../actions/errorActions";
 
 export default function LoginModal() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting, isValid, dirtyFields },
+  } = useForm({
+    mode: "onChange",
+  });
+
   const dispatch = useDispatch();
 
-  const [user, setUser] = useState({ user: "", password: "" });
   const [modal, setModal] = useState(false);
   const [msg, setMsg] = useState(null);
 
@@ -46,16 +55,9 @@ export default function LoginModal() {
     setModal(!modal);
   };
 
-  const onChange = (e) => {
-    setUser({ ...user, [e.target.name]: e.target.value });
+  const onSubmit = (data) => {
+    dispatch(login(data));
   };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    dispatch(login(user));
-  };
-
-  const { email, password } = user;
 
   return (
     <div>
@@ -67,33 +69,52 @@ export default function LoginModal() {
         <ModalHeader toggle={toggle}>Login</ModalHeader>
         <ModalBody>
           {msg ? <Alert color="danger">{msg}</Alert> : null}
-          <Form onSubmit={onSubmit}>
+          <Form onSubmit={handleSubmit(onSubmit)}>
             <FormGroup>
               <Label for="email">Email</Label>
               <Input
                 type="email"
-                name="email"
                 id="email"
-                value={email}
                 placeholder="Email"
                 className="mb-3"
-                onChange={onChange}
-                required
+                invalid={!!errors.email}
+                valid={!errors.email && dirtyFields.email}
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value:
+                      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    message: "Please enter a valid email",
+                  },
+                })}
               />
+              <FormFeedback invalid>{errors.email?.message}</FormFeedback>
 
               <Label for="password">Password</Label>
               <Input
                 type="password"
                 name="password"
                 id="password"
-                value={password}
                 placeholder="Password"
                 className="mb-3"
-                onChange={onChange}
-                minlength="6"
+                invalid={!!errors.password}
+                valid={!errors.password && dirtyFields.password}
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must have at least 6 characters",
+                  },
+                  pattern: {
+                    message:
+                      "Please enter a valid password with at least 6 chars",
+                  },
+                })}
               />
+              <FormFeedback invalid>{errors.password?.message}</FormFeedback>
+
               <Button
-                disabled={!email || !password}
+                disabled={isSubmitting || !isValid}
                 color="dark"
                 style={{ marginTop: "2rem" }}
                 block
--- client/src/components/auth/RegisterModal.js
+++ client/src/components/auth/RegisterModal.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
 import {
   Button,
   Modal,
@@ -8,17 +10,28 @@ import {
   FormGroup,
   Label,
   Input,
+  FormFeedback,
   NavLink,
   Alert,
 } from "reactstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { register } from "../../actions/authActions";
+import { register as registerUser } from "../../actions/authActions";
 import { clearErrors } from "../../actions/errorActions";
 
 export default function RegisterModal() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting, isValid, dirtyFields },
+    watch,
+  } = useForm({
+    mode: "onChange",
+  });
+
+  const password = useRef({});
+  password.current = watch("password", "");
+
   const dispatch = useDispatch();
 
-  const [user, setUser] = useState({ name: "", user: "", password: "" });
   const [modal, setModal] = useState(false);
   const [msg, setMsg] = useState(null);
 
@@ -46,16 +59,9 @@ export default function RegisterModal() {
     }
   }, [error, isAuthenticated, modal]);
 
-  const onChange = (e) => {
-    setUser({ ...user, [e.target.name]: e.target.value });
+  const onSubmit = (data) => {
+    dispatch(registerUser(data));
   };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    dispatch(register(user));
-  };
-
-  const { name, email, password } = user;
 
   return (
     <div>
@@ -67,47 +73,65 @@ export default function RegisterModal() {
         <ModalHeader>Register</ModalHeader>
         <ModalBody>
           {msg ? <Alert color="danger">{msg}</Alert> : null}
-          <Form onSubmit={onSubmit}>
+          <Form onSubmit={handleSubmit(onSubmit)}>
             <FormGroup>
               <Label for="name">Name</Label>
               <Input
                 type="text"
                 name="name"
                 id="name"
-                value={name}
                 placeholder="Name"
                 className="mb-3"
-                onChange={onChange}
-                minlength="2"
-                required
+                invalid={!!errors.name}
+                valid={!errors.name && dirtyFields.name}
+                {...register("name", {
+                  required: "Name is required",
+                })}
               />
+              <FormFeedback invalid>{errors.name?.message}</FormFeedback>
 
               <Label for="email">Email</Label>
               <Input
-                type="email"
-                name="email"
                 id="email"
-                value={email}
                 placeholder="Email"
                 className="mb-3"
-                onChange={onChange}
-                required
+                invalid={!!errors.email}
+                valid={!errors.email && dirtyFields.email}
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value:
+                      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    message: "Please enter a valid email",
+                  },
+                })}
               />
+              <FormFeedback invalid>{errors.email?.message}</FormFeedback>
 
               <Label for="password">Password</Label>
               <Input
                 type="password"
-                name="password"
                 id="password"
-                value={password}
                 placeholder="Password"
                 className="mb-3"
-                onChange={onChange}
-                minlength="6"
-                required
+                invalid={!!errors.password}
+                valid={!errors.password && dirtyFields.password}
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must have at least 6 characters",
+                  },
+                  pattern: {
+                    message:
+                      "Please enter a valid password with at least 6 chars",
+                  },
+                })}
               />
+              <FormFeedback invalid>{errors.password?.message}</FormFeedback>
+
               <Button
-                disabled={!name || !email || !password}
+                disabled={!isValid || isSubmitting}
                 color="dark"
                 style={{ marginTop: "2rem" }}
                 block
